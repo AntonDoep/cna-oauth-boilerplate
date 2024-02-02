370bc06059a250a893e624bca10fa69fc85a78c9
AntonDoep <doepelca@arcada.fi>
2024-02-02 11:17:36 +0200

    funkar je je

--- index.js
+++ index.js
@@ -6,8 +6,67 @@ const port = process.env.PORT || 3000
 const app = express()
 
 app.get("/", (req, res) => {
-    return res.send(`<h1>Hello!</h1>`)
+    if(!req.query.token){
+    return res.send(`<h1>Hello!</h1>
+    <a href="./auth"> logga in med github </a>
+    `)
+    }
+    try {
+        const user = jwt.verify(req.query.token, process.env.JWT_SECRET)
+        res.send(`
+        <h1> Welcome ${user.username}!</h1>`)
+    }catch (err){
+        res.send('Authorization failed!')
+    }
 });
+
+app.get("/auth", (req, res) => {
+    const params = {
+        scope: "read:user",
+        client_id: process.env.CLIENT_ID
+    }
+
+    const urlParams = new URLSearchParams(params).toString()
+
+    res.redirect(`https://github.com/login/oauth/authorize?${urlParams}`)
+})
+app.get("/github-callback", async (req, res) => {
+    const body = {
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+        code: req.query.code
+    }
+
+    const response = await fetch ("https://github.com/login/oauth/access_token", {
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/json',
+            'Accept' : 'application/json'
+                
+        },
+        body: JSON.stringify(body)
+    
+    })
+
+    const data = await response.json()
+    console.log(data)
+
+    const resUser = await fetch("https://api.github.com/user",{
+        headers: { 'Authorization': `Bearer ${data.access_token}`}
+    })
+
+    const userData = await resUser.json()
+
+    console.log(userData)
+
+    const localToken = jwt.sign({
+        sub: userData.id,
+        username: userData.login,
+        name: userData.name
+    },process.env.JWT_SECRET, {expiresIn:'1h'})
+
+    res.redirect(`/?token=${localToken}`)
+})
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`)
